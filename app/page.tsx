9c22c44984c12a6e3f85361098889cecb1031d71
Your Name <you@example.com>
2024-11-21 08:38:18 -0700

    #9 - Created Featured Header Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Navigation } from "./components/navigation/navigation";
+import { FeaturedHeader } from "./components/featured-header/featured-header";
 export default function Home() {
   return (
     <main>
       <Navigation />
+      <FeaturedHeader />
     </main>
   );
 }
