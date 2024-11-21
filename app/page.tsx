feb3c24e87ed3f962e213dda8d5ccb2d8bfa936b
Your Name <you@example.com>
2024-11-21 09:32:30 -0700

    #10 - Create Trusted Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { Navigation } from "./components/navigation/navigation";
+import { TrustedSection } from "./components/trusted-section/trusted-section";
 export default function Home() {
   return (
     <main>
       <Navigation />
+      <TrustedSection />
     </main>
   );
 }
