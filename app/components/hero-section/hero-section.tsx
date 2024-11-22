5fc6c73c6912b40eaf2c2ee0ee0506c4ac81815a
Lukas Tomasek <vedstav31@gmail.com>
2024-11-22 16:37:00 -0500

    #5 - support for responsive design

--- app/components/hero-section/hero-section.tsx
+++ app/components/hero-section/hero-section.tsx
@@ -26,41 +26,49 @@ export const HeroSection = () => {
     <source src="/assets/videos/hero.mp4#t=0.001" type="video/mp4" />
     Your browser does not support the video tag.
    </video>
-   <div className="absolute xs:top-0 xxs:top-0 lg:top-1/4 flex items-center justify-around w-full pointer-events-none">
-    <h1 className="lg:text-7xl xs:text-2xl text-center drop-shadow-sm text-white font-bold pointer-events-auto">
+   <div className="absolute xs:top-0 xxs:top-0 lg:top-1/4 flex items-center justify-around w-full pointer-events-none lg:my-0 xs:my-8 xxs:my-4">
+    <h1
+     className="lg:text-7xl lg:mx-0 xxs:mx-10 xs:text-3xl
+    text-center drop-shadow-sm text-white font-bold pointer-events-auto">
      All homes are for a lifetime. <br /> This one is once in a lifetime.
     </h1>
-    <IconButton
-     icon="/assets/icons/play_circle_filled.svg"
-     description="play"
-     width={isMobile ? 80 : 120}
-     height={isMobile ? 80 : 120}
-     onClick={onPlayButtonClicked}
-    />
-    <section className="flex flex-col gap-3 items-center">
-     <div className="bg-white w-1 h-20 lg:block "></div>
+    <div className="lg:block lg:top-0 xxs:relative xs:relative xxs:bottom-0 xs:top-24">
      <IconButton
-      icon="/assets/icons/fb-icon.svg"
-      description="facebook"
-      width={isMobile ? 40 : 50}
-      height={isMobile ? 40 : 50}
+      icon="/assets/icons/play_circle_filled.svg"
+      description="play"
+      width={isMobile ? 80 : 120}
+      height={isMobile ? 80 : 120}
       onClick={onPlayButtonClicked}
      />
-     <IconButton
-      icon="/assets/icons/twitter-icon.svg"
-      description="twitter"
-      width={isMobile ? 50 : 60}
-      height={isMobile ? 50 : 60}
-      onClick={onPlayButtonClicked}
-     />
-     <IconButton
-      icon="/assets/icons/ig-icon.svg"
-      description="isntagram"
-      width={isMobile ? 50 : 60}
-      height={isMobile ? 50 : 60}
-      onClick={onPlayButtonClicked}
-     />
-     <div className="bg-white w-1 h-20 lg:block "></div>
+    </div>
+    <section className="lg:flex lg:flex-col xs:grid xs:grid-cols-1 xxs:grid xxs:grid-cols-1 gap-3 items-center">
+     <div className="bg-white w-1 h-20 lg:block xs:hidden xxs:hidden"></div>
+     {!isMobile && (
+      <>
+       <IconButton
+        icon="/assets/icons/fb-icon.svg"
+        description="facebook"
+        width={isMobile ? 40 : 50}
+        height={isMobile ? 40 : 50}
+        onClick={onPlayButtonClicked}
+       />
+       <IconButton
+        icon="/assets/icons/twitter-icon.svg"
+        description="twitter"
+        width={isMobile ? 50 : 60}
+        height={isMobile ? 50 : 60}
+        onClick={onPlayButtonClicked}
+       />
+       <IconButton
+        icon="/assets/icons/ig-icon.svg"
+        description="isntagram"
+        width={isMobile ? 50 : 60}
+        height={isMobile ? 50 : 60}
+        onClick={onPlayButtonClicked}
+       />
+      </>
+     )}
+     <div className="bg-white w-1 h-20 lg:block xs:hidden xxs:hidden"></div>
     </section>
    </div>
    <div>
