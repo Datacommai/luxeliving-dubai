b0728f5e0cde808c23d416eb93ef06b760c38168
Your Name <you@example.com>
2024-11-22 10:43:26 -0700

    #11 - Create Footer Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import { Navigation } from './components/navigation/navigation';
-import { TrustedSection } from './components/trusted-section/trusted-section';
-import { FeaturedHeader } from './components/featured-header/featured-header';
-import { PropertySelection } from './components/property-selection/property-selection';
+import { Navigation } from "./components/navigation/navigation";
+import { TrustedSection } from "./components/trusted-section/trusted-section";
+import { FeaturedHeader } from "./components/featured-header/featured-header";
+import { PropertySelection } from "./components/property-selection/property-selection";
+import { Footer } from "./components/footer/footer";
 
 export default function Home() {
- return (
-  <main className="flex flex-col justify-center items-center">
-   <Navigation />
-   <PropertySelection />
-   <FeaturedHeader />
-   <TrustedSection />
-  </main>
- );
+  return (
+    <main className="flex flex-col justify-center items-center">
+      <Navigation />
+      <PropertySelection />
+      <FeaturedHeader />
+      <TrustedSection />
+      <Footer />
+    </main>
+  );
 }
