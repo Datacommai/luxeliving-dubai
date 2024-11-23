0f95e12bd4184cd4c7bcb90e6ee3a1321a78b48c
Lukas Tomasek <vedstav31@gmail.com>
2024-11-23 14:25:08 -0500

    #18 - support for mobile

--- app/components/featured-properties-section/featured-properties-section.tsx
+++ app/components/featured-properties-section/featured-properties-section.tsx
@@ -3,8 +3,8 @@ import { featuredPropertiesMockData } from '@/lib/mocks/featured-properties-mock
 
 export const FeaturedPropertiesSection = () => {
  return (
-  <section className="w-full max-h-[1000px] m-5 flex justify-center items-center">
-   <section className="w-full max-w-7xl grid grid-cols-3 gap-2 justify-items-center items-center">
+  <section className="w-full h-auto m-5 flex justify-center items-center">
+   <section className="w-full max-w-7xl grid lg:grid-cols-3 gap-2 justify-items-center items-center">
     {featuredPropertiesMockData.map((property, key) => (
      <FeaturedProperty
       key={key}
