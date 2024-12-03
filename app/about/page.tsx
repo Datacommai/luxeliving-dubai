848defd094f925217f1b856b7679c085cf71f24a
Your Name <you@example.com>
2024-12-02 17:44:51 -0700

    #35 - Create About Purpose Section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,7 @@ import { Navigation } from "../components/navigation/navigation";
 import { TrustedSection } from "../components/trusted-section/trusted-section";
 import { AboutHeader } from "../components/about-header/about-header";
 import { AboutUsSection } from "../components/about-us-section/about-us-section";
+import { AboutPurposeSection } from "../components/about-purpose-section/about-purpose-section";
 
 export default function About() {
   return (
@@ -10,7 +11,9 @@ export default function About() {
       <Navigation />
       <AboutHeader />
       <AboutUsSection />
+      <AboutPurposeSection />
       <TrustedSection />
+
       <Footer />
     </main>
   );
