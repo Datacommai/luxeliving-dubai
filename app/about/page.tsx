820f293aef2623b567ecbac5e87ab130d7f54e4d
Your Name <you@example.com>
2024-12-03 09:39:17 -0700

    #35 - removed duplicated purpose section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,7 +12,6 @@ export default function About() {
       <Navigation />
       <AboutHeader />
       <AboutUsSection />
-      <AboutPurposeSection />
       <AboutExpertsSection />
       <AboutPurposeSection />
       <TrustedSection />
