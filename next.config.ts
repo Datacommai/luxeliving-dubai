e0fcde15215d64244650bc5f74fec3a209fe2ea0
Lukas Tomasek <vedstav31@gmail.com>
2024-12-04 16:59:01 -0500

    #40 -implemented firebase

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+ /** if you want to test build comment-out output, deploying to Firebase output is required  */
+ output: 'export',
+ distDir: 'build',
 };
 
 export default nextConfig;
