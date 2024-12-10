c0d8c6d545ef39ea1dfc4b3638574eeee2397248
Your Name <you@example.com>
2024-12-10 07:38:08 -0700

    #47 - Create Privacy Policy Page

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,131 +1,134 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { Separator } from '@/components/ui/separator';
+import Link from "next/link";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 
 export const Footer = () => {
- return (
-  <>
-   <footer className="bg-[#1E3747] relative w-full xxs:gap-6 grid grid-col-1 xxs:py-14 xxs:px-6 md:py-18 lg:py-24 md:px-20 lg:px-32">
-    <section className="flex flex-col gap-6">
-     <ul className="md:flex xxs:grid-col xxs:grid-cols-1  text-center md:flex-row xxs:gap-4 md:gap-6">
-      <Link href="/">
-       <li className="text-lg text-white">Home</li>
-      </Link>
-      <Link href="/">
-       <li className="text-lg text-white">Projects</li>
-      </Link>
-      <Link href="/about">
-       <li className="text-lg text-white">About us</li>
-      </Link>
-     </ul>
-     <Separator className="w-full xxs:hidden md:block border-2 border-[#274659] border-solid" />
-    </section>
+  return (
+    <>
+      <footer className="bg-[#1E3747] relative w-full xxs:gap-6 grid grid-col-1 xxs:py-14 xxs:px-6 md:py-18 lg:py-24 md:px-20 lg:px-32">
+        <section className="flex flex-col gap-6">
+          <ul className="md:flex xxs:grid-col xxs:grid-cols-1  text-center md:flex-row xxs:gap-4 md:gap-6">
+            <Link href="/">
+              <li className="text-lg text-white">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="text-lg text-white">Projects</li>
+            </Link>
+            <Link href="/about">
+              <li className="text-lg text-white">About us</li>
+            </Link>
+          </ul>
+          <Separator className="w-full xxs:hidden md:block border-2 border-[#274659] border-solid" />
+        </section>
 
-    <section className="md:flex xxs:grid w-full xxs:grid-col-1 gap-6 xxs:justify-center md:flex-row md:justify-between items-center ">
-     <div className="grid grid-col-1 w-full md:justify-start xxs:justify-center items-center xxs:text-center md:text-left gap-1">
-      <figure className="flex md:justify-start xxs:justify-center">
-       <Image
-        className="xxs:w-[120px] mb-3 sm:w-[180px] sm:h-auto md:w-[220px] lg:h-[57px]"
-        width={220}
-        height={57}
-        src="/assets/logo/luxeliving-logo.svg"
-        alt="logo"
-        priority
-        blurDataURL="blur"
-        quality={100}
-       />
-      </figure>
-      <h2 className="text-white xxs:text-sm md:text-2xl font-bold">
-       AZOO Co., Ltd.
-      </h2>
-      <p className="text-white xxs:text-xs md:text-2xl">
-       Main Street, Apartment 4B, Cityville, <br />
-       State 54321, United States
-      </p>
-     </div>
-     <ul className="flex justify-center items-center gap-7">
-      <Link href="/">
-       <li>
-        <Image
-         className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
-         width={32}
-         height={32}
-         src="/assets/icons/facebook-icon.svg"
-         alt="facebook"
-         blurDataURL="blur"
-         quality={100}
-        />
-       </li>
-      </Link>
-      <Link href="/">
-       <li>
-        <Image
-         className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
-         width={32}
-         height={32}
-         src="/assets/icons/youtube-icon.svg"
-         alt="youtube"
-         blurDataURL="blur"
-         quality={100}
-        />
-       </li>
-      </Link>
-      <Link href="/">
-       <li>
-        <Image
-         className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
-         width={32}
-         height={32}
-         src="/assets/icons/linkedin-icon.svg"
-         alt="linkedin"
-         blurDataURL="blur"
-         quality={100}
-        />
-       </li>
-      </Link>
-      <Link href="/">
-       <li>
-        <Image
-         className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
-         width={32}
-         height={32}
-         src="/assets/icons/instagram-icon.svg"
-         alt="instagram"
-         blurDataURL="blur"
-         quality={100}
-        />
-       </li>
-      </Link>
-      <Link href="/">
-       <li>
-        <Image
-         className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
-         width={32}
-         height={32}
-         src="/assets/icons/x-icon.svg"
-         alt="x"
-         blurDataURL="blur"
-         quality={100}
-        />
-       </li>
-      </Link>
-     </ul>
-    </section>
-   </footer>
+        <section className="md:flex xxs:grid w-full xxs:grid-col-1 gap-6 xxs:justify-center md:flex-row md:justify-between items-center ">
+          <div className="grid grid-col-1 w-full md:justify-start xxs:justify-center items-center xxs:text-center md:text-left gap-1">
+            <figure className="flex md:justify-start xxs:justify-center">
+              <Image
+                className="xxs:w-[120px] mb-3 sm:w-[180px] sm:h-auto md:w-[220px] lg:h-[57px]"
+                width={220}
+                height={57}
+                src="/assets/logo/luxeliving-logo.svg"
+                alt="logo"
+                priority
+                blurDataURL="blur"
+                quality={100}
+              />
+            </figure>
+            <h2 className="text-white xxs:text-sm md:text-2xl font-bold">
+              AZOO Co., Ltd.
+            </h2>
+            <p className="text-white xxs:text-xs md:text-2xl">
+              Main Street, Apartment 4B, Cityville, <br />
+              State 54321, United States
+            </p>
+          </div>
+          <ul className="flex justify-center items-center gap-7">
+            <Link href="/">
+              <li>
+                <Image
+                  className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
+                  width={32}
+                  height={32}
+                  src="/assets/icons/facebook-icon.svg"
+                  alt="facebook"
+                  blurDataURL="blur"
+                  quality={100}
+                />
+              </li>
+            </Link>
+            <Link href="/">
+              <li>
+                <Image
+                  className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
+                  width={32}
+                  height={32}
+                  src="/assets/icons/youtube-icon.svg"
+                  alt="youtube"
+                  blurDataURL="blur"
+                  quality={100}
+                />
+              </li>
+            </Link>
+            <Link href="/">
+              <li>
+                <Image
+                  className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
+                  width={32}
+                  height={32}
+                  src="/assets/icons/linkedin-icon.svg"
+                  alt="linkedin"
+                  blurDataURL="blur"
+                  quality={100}
+                />
+              </li>
+            </Link>
+            <Link href="/">
+              <li>
+                <Image
+                  className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
+                  width={32}
+                  height={32}
+                  src="/assets/icons/instagram-icon.svg"
+                  alt="instagram"
+                  blurDataURL="blur"
+                  quality={100}
+                />
+              </li>
+            </Link>
+            <Link href="/">
+              <li>
+                <Image
+                  className="md:w-8 md:h-8 xxs:w-5 xxs:h-5"
+                  width={32}
+                  height={32}
+                  src="/assets/icons/x-icon.svg"
+                  alt="x"
+                  blurDataURL="blur"
+                  quality={100}
+                />
+              </li>
+            </Link>
+          </ul>
+        </section>
+      </footer>
 
-   <section className="flex xxs:flex-col md:px-20 gap-2 md:py-4 lg:px-32 w-full p-6 bg-[#0F202A] xxs:justify-center items-center md:justify-between md:flex-row">
-    <p className="xxs:text-xs md:text-base text-white md:font-bold">
-     AZ00.Inc. All rights reserved.
-    </p>
-    <span className="flex gap-8">
-     <Link className=" xxs:text-xs md-text-base text-white" href="/">
-      Terms of use
-     </Link>
-     <Link className=" xxs:text-xs md-text-base text-white" href="/">
-      Privacy Policy
-     </Link>
-    </span>
-   </section>
-  </>
- );
+      <section className="flex xxs:flex-col md:px-20 gap-2 md:py-4 lg:px-32 w-full p-6 bg-[#0F202A] xxs:justify-center items-center md:justify-between md:flex-row">
+        <p className="xxs:text-xs md:text-base text-white md:font-bold">
+          AZ00.Inc. All rights reserved.
+        </p>
+        <span className="flex gap-8">
+          <Link className=" xxs:text-xs md-text-base text-white" href="/">
+            Terms of use
+          </Link>
+          <Link
+            className=" xxs:text-xs md-text-base text-white"
+            href="/privacy-policy"
+          >
+            Privacy Policy
+          </Link>
+        </span>
+      </section>
+    </>
+  );
 };
---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,176 @@
+import { Footer } from "../components/footer/footer";
+import { Separator } from "@/components/ui/separator";
+
+import Link from "next/link";
+
+export default function PrivacyPolicy() {
+  return (
+    <main className="h-full">
+      <section className="mx-12 my-2">
+        <h2 className="text-black font-bold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          Privacy Policy
+        </h2>
+        <article className="my-5 xs:text-sm md:text-lg">
+          At LuxeLivingDubai, we are committed to protecting your privacy. This
+          policy outlines how we collect, use, and safeguard your personal
+          information to provide you with the best possible services.
+        </article>
+      </section>
+      <Separator />
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          1. Collection of Personal Information
+        </h2>
+        <article className="my-5 xs:text-sm md:text-lg">
+          To contact you and deliver our services effectively, we may collect
+          the following personal information:
+          <br />
+          <p className="my-5 font-bold">
+            Your name, address, phone number, and email address when you:
+          </p>
+          <ol className="space-y-2 font-light">
+            <li>- Register on our website.</li>
+            <li>- Inquire about our products or services.</li>
+            <li>- Subscribe to our newsletter or marketing communications.</li>
+          </ol>
+          <p className="my-2 font-medium xs:text-sm md:text-lg">
+            We only collect personal information necessary for providing our
+            services. Sensitive information will only be collected with your
+            explicit consent.
+          </p>
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          2. Use of Personal Information
+        </h2>
+        <article className="my-5  xs:text-sm md:text-lg">
+          We use your personal information to:
+          <ol className="space-y-2 my-5 font-light">
+            <li>- Respond to inquiries and provide requested services.</li>
+            <li>
+              - Improve your experience on our website and personalize our
+              services.
+            </li>
+            <li>
+              - Send marketing communications about our services or third-party
+              products with your consent.
+            </li>
+          </ol>
+          <p className=" font-medium xs:text-sm md:text-lg">
+            You will always have the option to opt out of such communications.
+          </p>
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          3. Marketing and Third Parties
+        </h2>
+        <article className="my-5 xs:text-sm md:text-lg">
+          <ol className="space-y-2 font-light">
+            <li>
+              - With your explicit permission, we may share your details with
+              trusted third parties for marketing purposes.
+            </li>
+            <li>
+              - You will always have the opportunity to refuse such
+              communications by following the unsubscribe instructions in our
+              emails.
+            </li>
+          </ol>
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          4. Use of Cookies
+        </h2>
+        <article className="my-5 xs:text-sm md:text-lg">
+          This website uses cookies, tracking pixels, and related technologies
+          to enhance user experience. These small data files:
+          <ol className="space-y-2 my-5 font-light">
+            <li>- Help us operate and personalize the website.</li>
+            <li>
+              - Enable tracking of your activity on the site to deliver targeted
+              ads on other platforms.
+            </li>
+          </ol>
+          <p className="font-medium xs:text-sm md:text-lg">
+            You can manage or disable cookies through your browser settings.
+            Note that disabling cookies may impact the functionality of certain
+            features on the site.
+          </p>
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          5. Data Protection and Security
+        </h2>
+        <article className="my-5  xs:text-sm md:text-lg">
+          We are committed to ensuring your personal data is secure. We
+          implement appropriate technical and organizational measures to prevent
+          unauthorized access, alteration, or disclosure of your information.
+          However, no system can guarantee absolute security, and you use the
+          site at your own risk.
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          6. Your Rights
+        </h2>
+
+        <article className="my-5 xs:text-sm md:text-lg">
+          You have the following rights regarding your personal data:
+          <ol className="space-y-2 my-5 font-light">
+            <li>- Access your personal data and request corrections.</li>
+            <li>
+              - Withdraw your consent for marketing or other data processing
+              activities.
+            </li>
+            <li>
+              - Request deletion of your personal data unless required for legal
+              or operational purposes.
+            </li>
+          </ol>
+          <p className="font-medium xs:text-sm md:text-lg">
+            To exercise these rights, contact us using the details below.
+          </p>
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          7. Changes to This Policy
+        </h2>
+        <article className="my-5  xs:text-sm md:text-lg">
+          We reserve the right to update this Privacy Policy at any time.
+          Changes will be posted on this page with an updated "Last Revised"
+          date. Continued use of our website constitutes your agreement to the
+          revised terms.
+        </article>
+      </section>
+
+      <section className="mx-12 my-8">
+        <h2 className="text-black font-semibold xs:text-xl md:text-2xl xs:leading-tight md:leading-tight">
+          8. Contact Us
+        </h2>
+        <article className="my-5  xs:text-sm md:text-lg">
+          If you have any questions about this Privacy Policy or wish to stop
+          receiving communications from us, please contact us at:
+          <Link
+            className="underline text-success gap-1"
+            href="mailto: info@luxelivingdubai.com"
+          >
+            info@luxelivingdubai.com
+          </Link>
+        </article>
+      </section>
+      <Footer />
+    </main>
+  );
+}
