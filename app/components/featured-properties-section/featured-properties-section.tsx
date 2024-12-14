06d14362b69daeec89682a63d8ae29048ae2ace7
Lukas Tomasek <vedstav31@gmail.com>
2024-12-14 11:55:32 -0500

    #52 - Landing page improvements part-2

--- app/components/featured-properties-section/featured-properties-section.tsx
+++ app/components/featured-properties-section/featured-properties-section.tsx
@@ -3,8 +3,8 @@ import { featuredPropertiesMockData } from '@/lib/mocks/featured-properties-mock
 
 export const FeaturedPropertiesSection = () => {
  return (
-  <section className="w-full h-auto m-5 flex justify-center items-center">
-   <section className="w-full max-w-7xl grid lg:grid-cols-3 gap-2 justify-items-center items-center">
+  <section className="w-full h-auto m-5 flex justify-center items-center gap-2">
+   <section className="w-full max-w-7xl grid xl:grid-cols-3 lg:grid-cols-2 gap-2 justify-items-center items-center">
     {featuredPropertiesMockData.map((property, key) => (
      <FeaturedProperty
       key={key}
