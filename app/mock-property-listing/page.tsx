19917c56c1014d1dd72eb5c8b542c00431669a7b
Lukas Tomasek <vedstav31@gmail.com>
2024-12-21 13:01:02 -0500

    fixed merge conflicts

--- app/mock-property-listing/page.tsx
+++ app/mock-property-listing/page.tsx
@@ -1,11 +1,12 @@
 import { Navigation } from '../components/navigation/navigation';
 import PropertyListingHeroWithDelay from '../components/property-listing-components/property-listing-hero/property-listing-hero';
 import { FeatureEnabled } from '../feature-enabled/feature-enabled';
-import PropertyListingGalleryWithDelay from '../components/property-listing-gallery/property-listing-gallery';
-import PropertyListingDetailsWithDelay from '../components/property-listing-details/property-listing-details';
-import PropertyAmenitiesWithDelay from '../components/property-amenities/property-amenities';
 import { PropertyListingTabbedGalleryWithDelay } from '../components/property-listing-components/property-listing-tabbed-gallery/property-listing-tabbed-gallery';
 import { Footer } from '../components/footer/footer';
+import PropertyListingGalleryWithDelay from '../components/property-listing-components/property-listing-gallery/property-listing-gallery';
+import PropertyAmenitiesWithDelay from '../components/property-listing-components/property-listing-amenities/property-listing-amenities';
+import PropertyListingDetailsWithDelay from '../components/property-listing-components/property-listing-details/property-listing-details';
+import PropertyListingAboutWithDelay from '../components/property-listing-components/property-listing-about/property-listing-about';
 
 export default function MockPropertyListing() {
  return (
@@ -16,6 +17,7 @@ export default function MockPropertyListing() {
     <PropertyListingGalleryWithDelay />
     <PropertyAmenitiesWithDelay />
     <PropertyListingDetailsWithDelay />
+    <PropertyListingAboutWithDelay />
     <PropertyListingTabbedGalleryWithDelay />
     <Footer />
    </main>
