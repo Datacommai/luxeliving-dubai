e9c5cfcf285fec0dfdd6c7a7d875337b91394ec0
Lukas Tomasek <vedstav31@gmail.com>
2024-12-21 12:56:06 -0500

    fixes

--- app/mock-property-listing/page.tsx
+++ app/mock-property-listing/page.tsx
@@ -1,22 +1,22 @@
-import { Navigation } from "../components/navigation/navigation";
-import PropertyListingHeroWithDelay from "../components/property-listing-components/property-listing-hero/property-listing-hero";
-import { FeatureEnabled } from "../feature-enabled/feature-enabled";
-import PropertyListingGalleryWithDelay from "../components/property-listing-components/property-listing-gallery/property-listing-gallery";
-import PropertyListingDetailsWithDelay from "../components/property-listing-components/property-listing-details/property-listing-details";
-import PropertyAmenitiesWithDelay from "../components/property-listing-components/property-listing-amenities/property-listing-amenities";
-import PropertyListingAboutWithDelay from "../components/property-listing-components/property-listing-about/property-listing-about";
+import { Navigation } from '../components/navigation/navigation';
+import PropertyListingHeroWithDelay from '../components/property-listing-components/property-listing-hero/property-listing-hero';
+import { FeatureEnabled } from '../feature-enabled/feature-enabled';
+import PropertyListingGalleryWithDelay from '../components/property-listing-components/property-listing-gallery/property-listing-gallery';
+import PropertyListingDetailsWithDelay from '../components/property-listing-components/property-listing-details/property-listing-details';
+import PropertyAmenitiesWithDelay from '../components/property-listing-components/property-listing-amenities/property-listing-amenities';
+import PropertyListingAboutWithDelay from '../components/property-listing-components/property-listing-about/property-listing-about';
 
 export default function MockPropertyListing() {
-  return (
-    <FeatureEnabled featureFlag="DEMO_FEATURE">
-      <main className="flex flex-col justify-center items-center">
-        <Navigation />
-        <PropertyListingHeroWithDelay />
-        <PropertyListingGalleryWithDelay />
-        <PropertyAmenitiesWithDelay />
-        <PropertyListingDetailsWithDelay />
-        <PropertyListingAboutWithDelay />
-      </main>
-    </FeatureEnabled>
-  );
+ return (
+  <FeatureEnabled featureFlag="DEMO_FEATURE">
+   <main className="flex flex-col justify-center items-center">
+    <Navigation />
+    <PropertyListingHeroWithDelay />
+    <PropertyListingGalleryWithDelay />
+    <PropertyAmenitiesWithDelay />
+    <PropertyListingDetailsWithDelay />
+    <PropertyListingAboutWithDelay />
+   </main>
+  </FeatureEnabled>
+ );
 }
