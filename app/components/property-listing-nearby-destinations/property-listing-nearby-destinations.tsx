ebe42b7a8715dfd35d8884b1f73a36c7f12154ac
Your Name <you@example.com>
2024-12-20 17:20:18 -0700

    #63 - fixed enums and object array and list spacing

--- app/components/property-listing-nearby-destinations/property-listing-nearby-destinations.tsx
+++ app/components/property-listing-nearby-destinations/property-listing-nearby-destinations.tsx
@@ -34,7 +34,6 @@ export default function PropertyListingNearbyDestinationsWithDelay() {
 function PropertyNearbyDestinationsSkeleton() {
   return (
     <section className="w-full 2xl:px-28 xxs:px-4 xxs:py-8 md:py-14 lg:py-20 md:px-10 lg:px-6 xl:px-16 flex flex-col justify-center items-center">
-      {/* Skeleton for Header Section */}
       <section className="flex flex-col justify-center text-center xxs:gap-3 md:gap-6 w-full">
         <div className="flex justify-center items-center xxs:gap-2 md:gap-6">
           <Skeleton className="xxs:h-0.5 md:h-1 rounded xxs:w-[40px] md:w-[100px]" />
@@ -44,20 +43,13 @@ function PropertyNearbyDestinationsSkeleton() {
         <Skeleton className="xxs:h-6 sm:h-10 lg:h-12 xxs:w-[200px] sm:w-[300px] lg:w-[500px]" />
         <Skeleton className="xxs:h-4 md:h-6 xxs:w-[160px] md:w-[240px]" />
       </section>
-
-      {/* Skeleton for Content Section */}
       <section className="lg:grid xxs:flex flex-col lg:grid-cols-2 gap-4 mt-10 w-full">
-        {/* Left Image Skeleton */}
         <Skeleton className="w-full md:w-full md:h-[300px] xl:w-[632px] xl:h-[356px] 2xl:w-full 2xl:h-full rounded-lg shadow-lg" />
-
-        {/* Text Content Skeleton */}
         <div>
           <Skeleton className="xxs:h-4 md:h-6 w-full xxs:w-[200px] md:w-[320px]" />
           <Skeleton className="xxs:h-4 md:h-6 w-full xxs:w-[180px] md:w-[280px] mt-2" />
           <Skeleton className="xxs:h-4 md:h-6 w-full xxs:w-[250px] md:w-[380px] mt-2" />
         </div>
-
-        {/* Buttons Skeleton */}
         <div className="flex flex-col md:justify-center gap-4 justify-center text-center xxs:order-4 items-center xxs:order-4 lg:justify-between w-full">
           <div>
             <Skeleton className="xxs:h-4 md:h-6 w-full xxs:w-[200px] md:w-[320px]" />
@@ -69,22 +61,23 @@ function PropertyNearbyDestinationsSkeleton() {
             <Skeleton className="xxs:h-12 lg:h-[52px] lg:w-[322px] xxs:w-full rounded-md" />
           </span>
         </div>
-
-        {/* Right Image Skeleton */}
         <Skeleton className="w-full md:w-full md:h-[300px] xl:w-[632px] xl:h-[356px] lg:order-4 2xl:w-full 2xl:h-full rounded-lg shadow-lg" />
       </section>
     </section>
   );
 }
 
+export enum LocationType {
+  DOWN_TOWN = "DOWN_TOWN",
+  MARINA = "MARINA",
+  AIRPORT = "AIRPORT",
+}
+
 export type PropertyListingNearbyDestinationsProps = {
   keyBenefits: string[];
-  locations: Array<{
-    type: "DOWN_TOWN" | "MARINA" | "AIRPORT";
-    title: string;
-    icon: string;
-    duration: string;
-  }>;
+  locations: [
+    { type: LocationType; title: string; icon: string; duration: string }
+  ];
 };
 
 function PropertyListingNearbyDestinations(
@@ -92,20 +85,19 @@ function PropertyListingNearbyDestinations(
 ) {
   const { keyBenefits, locations } = props;
 
-  const iconTypes: Record<string, string> = {
-    DOWN_TOWN: "/assets/featured-properties-temp/downtown.svg",
-    MARINA: "/assets/featured-properties-temp/marina.svg",
-    AIRPORT: "/assets/featured-properties-temp/airport.svg",
+  const iconTypes: Record<LocationType, string> = {
+    [LocationType.DOWN_TOWN]: "/assets/featured-properties-temp/downtown.svg",
+    [LocationType.MARINA]: "/assets/featured-properties-temp/marina.svg",
+    [LocationType.AIRPORT]: "/assets/featured-properties-temp/airport.svg",
   };
 
-  // Filter out locations with missing data
   const validLocations = locations.filter(
     (location) =>
       location &&
       location.type &&
       location.title &&
       location.duration &&
-      iconTypes[location.type] // Ensure icon type exists
+      iconTypes[location.type]
   );
 
   return (
@@ -131,17 +123,15 @@ function PropertyListingNearbyDestinations(
             className={`xxs:h-0.5 md:h-1 rounded xxs:w-[40px] md:w-[100px] ${styles.separator}`}
           />
         </span>
-
         <h1 className="xs:text-xl sm:text-3xl lg:text-5xl font-bold text-[#212121]">
           Nearby Destinations
         </h1>
-        <p className="xxs:text-xs md:text-xl text-[#434343]">
+        <p className="xxs:text-xs md:text-xl text-[#434343] tracking-wide">
           Effortlessly plan trips with real-time travel estimates to key spots
           nearby. See how many minutes it takes to reach the airport, city
           center, and more, helping you stay on schedule and make the most of
           your time—all at a quick glance.
         </p>
-
         <section className="grid-cols-2 lg:hidden xxs:grid justify-center justify-items-center items-center md:gap-6 xxs:gap-3">
           {validLocations.map((location, index) => (
             <CloseByCard
@@ -152,11 +142,10 @@ function PropertyListingNearbyDestinations(
             />
           ))}
         </section>
-
         <h3 className="text-[#333333] xxs:pl-2 lg:pl-0 text-left font-medium md:text-2xl">
           Key Benefits
         </h3>
-        <ul className="gap-3 list-disc text-left pl-10">
+        <ul className="space-y-3 list-disc text-left pl-7">
           {keyBenefits?.map((benefit, index) => (
             <li key={index} className="text-[#434343] md:text-xl">
               {benefit}
@@ -178,7 +167,7 @@ function CloseByCard({
   cardDuration: string;
 }) {
   return (
-    <div className="flex flex-col items-start w-full h-full rounded-[6px] md:gap-6 xxs:p-3 md:py-6 md:px-4 2xl:px-8 xxs:gap-3 bg-white">
+    <div className="flex flex-col shadow-md items-start w-full h-full rounded-[6px] md:gap-6 xxs:p-3 md:py-6 md:px-4 2xl:px-8 xxs:gap-3 bg-white">
       <Image
         className="xxs:w-7 xxs:h-7 md:w-12 md:h-12"
         width={48}
