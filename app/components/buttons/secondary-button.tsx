a1cd3c389ff75b703cb409d684af9cc6e4befd6b
Your Name <ahmedheafmohamed641@gmail.com>
2025-01-07 14:49:30 -0700

    #82 - Fix Button And Any Other Sizing For-375px And Below

--- app/components/buttons/secondary-button.tsx
+++ app/components/buttons/secondary-button.tsx
@@ -1,43 +1,45 @@
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 
 type SecondaryButtonProps = {
- title: string;
- onClick?: () => void;
- variant: 'filled' | 'outlined';
- width?: number;
- height?: number;
+  title: string;
+  onClick?: () => void;
+  variant: "filled" | "outlined";
+  width?: number;
+  height?: number;
+  className?: string;
 };
 
 export const SecondaryButton = (props: SecondaryButtonProps) => {
- const { title, onClick, variant, width, height } = props;
+  const { title, onClick, variant, width, height, className } = props;
 
- const createStyles = () => {
-  switch (variant) {
-   case 'filled':
-    return {
-     width: `${width}px`,
-     height: `${height}px`,
-     backgroundColor: '#D1AF49',
-     fontWeight: 700,
-    };
-   case 'outlined':
-    return {
-     width: `${width}px`,
-     height: `${height}px`,
-     backgroundColor: 'transparent',
-     border: '1px solid #D1AF49',
-     fontWeight: 400,
-    };
-  }
- };
+  const createStyles = () => {
+    switch (variant) {
+      case "filled":
+        return {
+          width: `${width}px`,
+          height: `${height}px`,
+          backgroundColor: "#D1AF49",
+          fontWeight: 700,
+        };
+      case "outlined":
+        return {
+          width: `${width}px`,
+          height: `${height}px`,
+          backgroundColor: "transparent",
+          border: "1px solid #D1AF49",
+          fontWeight: 400,
+        };
+    }
+  };
 
- return (
-  <Button
-   onClick={onClick}
-   style={createStyles()}
-   className="rounded-lg text-[#333333]"
-   variant="default">
-   {title}
-  </Button>
- );
+  return (
+    <Button
+      onClick={onClick}
+      style={createStyles()}
+      className={`rounded-lg text-[#333333] ${className || ""}`}
+      variant="default"
+    >
+      {title}
+    </Button>
+  );
 };
