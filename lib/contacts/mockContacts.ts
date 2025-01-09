6188f2ff00db004f26415ce1db5c0a8c97a642ec
Your Name <ahmedheafmohamed641@gmail.com>
2025-01-09 10:38:49 -0700

    #88 - Create Contacts Object

---
+++ lib/contacts/mockContacts.ts
@@ -0,0 +1,16 @@
+import { ContactsType } from "./types";
+
+export const mockContact: ContactsType = {
+  fullname: "AZOO Co., Ltd",
+  tel: 1234567890,
+  email: "johndoe@example.com",
+  socials: {
+    facebook: "https://facebook.com/johndoe",
+    instagram: "https://instagram.com/johndoe",
+    linkedin: "https://linkedin.com/in/johndoe",
+    youtube: "https://youtube.com/johndoe",
+    x: "https://twitter.com/johndoe",
+  },
+  address: "Main Street, Apartment 4B, Cityville,",
+  city: "State 54321",
+};
